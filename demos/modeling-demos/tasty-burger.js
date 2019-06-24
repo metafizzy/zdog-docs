b41dcacd4de506e7f7a9f7666799bf691383107d
David DeSandro <desandrocodes@gmail.com>
2019-06-24 09:04:53 -0400

    🎩 tighter burger; 📝 zdog experiments

--- demos/modeling-demos/tasty-burger.js
+++ demos/modeling-demos/tasty-burger.js
@@ -12,7 +12,7 @@ ZdogDocs.tastyBurger = ZdogDocs.shapeDemo( function( canvas, data, illo ) {
   var topBun = new Zdog.Hemisphere({
     addTo: burger,
     diameter: 96,
-    translate: { z: 54 },
+    translate: { z: 44 },
     stroke: 24,
     color: orange,
     // backface: gold,
@@ -23,7 +23,7 @@ ZdogDocs.tastyBurger = ZdogDocs.shapeDemo( function( canvas, data, illo ) {
     addTo: burger,
     width: 92,
     height: 92,
-    translate: { z: 28 },
+    translate: { z: 24 },
     stroke: 16,
     color: yellow,
     fill: true,
@@ -43,7 +43,7 @@ ZdogDocs.tastyBurger = ZdogDocs.shapeDemo( function( canvas, data, illo ) {
     addTo: burger,
     diameter: topBun.diameter,
     length: 16,
-    translate: { z: -40 },
+    translate: { z: -36 },
     stroke: topBun.stroke,
     color: topBun.color,
   });
